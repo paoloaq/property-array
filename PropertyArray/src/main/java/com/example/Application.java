0cca0a73d7360283427e7e74be8d5933d5e91ef4
Paolo Vanacore <paolo.vanacore@gmail.com>
2016-12-31 15:17:35 +0100

    Solved

--- PropertyArray/src/main/java/com/example/Application.java
+++ PropertyArray/src/main/java/com/example/Application.java
@@ -1,12 +1,12 @@
 package com.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,41 +19,31 @@ public class Application {
 }
 
 @RestController
-class WithEnvCtrl {
-
-	@Autowired
-	private Environment env;
-
-	@RequestMapping(value = "/with_env", method = { RequestMethod.GET,
-			RequestMethod.POST }, produces = "application/json")
-	public String test() {
-
-		System.err.println(env.getProperty("this.is.array[0]"));
-		System.err.println(env.getProperty("this.is.array", List.class));
-		System.err.println(env.getProperty("this.is.array", String[].class));
-
-		return env.getProperty("this.is.array[0]");
-	}
-}
-
-@RestController
+@ConfigurationProperties(prefix="this.is")
 class WithValueAnnotation {
 
-	@Value("${this.is.array[0]}")
-	private String first;
-	@Value("${this.is.array}")
-	private List<String> list;
-	@Value("${this.is.array}")
-	private String[] array;
-
+	private List<String> array = new ArrayList<>();
+	private int intero;
+	
+	public List<String> getArray(){
+		return this.array;
+	}
+	
+	public int getIntero() {
+		return this.intero;
+	}
+	
+	public void setIntero(int intero) {
+		this.intero = intero;
+	}
+	
 	@RequestMapping(value = "/with_value_annotation", method = { RequestMethod.GET,
 			RequestMethod.POST }, produces = "application/json")
 	public String test() {
 
-		System.err.println(first);
-		System.err.println(list);
 		System.err.println(array);
-
-		return first;
+		System.err.println(intero);
+		
+		return array.toString();
 	}
 }
